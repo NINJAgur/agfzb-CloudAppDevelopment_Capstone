689b57fd9f8eceaa5f59335a85c18b1f52389b57
name <yourgithub@email.com>
2023-04-01 10:38:54 -0400

    lab 2

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,20 +10,14 @@ urlpatterns = [
     # name the URL
 
     path('about/', view = views.about, name='about'),
-
-    # path for contact us view
     path('contact/', view = views.contact, name='contact'),
-
-    # path for registration
-
-    # path for login
-
-    # path for logout
-
+    path('signup/', views.registration_request, name='registration'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
 
     # path for add a review view
-
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
